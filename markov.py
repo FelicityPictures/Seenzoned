baa743022990f418ba024b58a4da88405874c4ea
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-11-11 08:46:07 -0500

    pretty much done :smile:

--- markov.py
+++ markov.py
@@ -1,4 +1,40 @@
+import random
 
-f = open("data.txt", 'r').read()
+END_OF_SENTENCE_CHARS = ['.', '?', '!']
 
-print f
+f = open("fellowship.txt", 'r').read().split('\n')
+
+markov = {}
+
+for i in f:
+    words = i.split()
+    if len(words) > 2:
+        for j in range(0, len(words) - 2):
+            if (words[j], words[j+1]) in markov:
+                markov[words[j], words[j+1]].append(words[j+2])
+            else:
+                markov[words[j], words[j+1]] = [words[j+2]]
+        if (words[-2], words[-1]) in markov:
+            markov[words[-2], words[-1]].append('.')
+        else:
+            markov[words[-2], words[-1]] = ['.']
+
+sentence = []
+
+key = list(random.choice(markov.keys()))
+
+while key[0][0].islower(): # makes sure it starts on a capital string
+    key = list(random.choice(markov.keys()))
+
+value = random.choice(markov[key[0], key[1]])
+
+sentence += key
+sentence.append(value)
+
+while (key[0], key[1]) in markov and value[-1] not in END_OF_SENTENCE_CHARS:
+    key[0] = key[1]
+    key[1] = value
+    value = random.choice(markov[key[0], key[1]])
+    sentence.append(value)
+
+print " ".join(sentence)
