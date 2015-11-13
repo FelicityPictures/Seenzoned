0c03c6b19a5d887dae8c1026761165e17a431c43
captiosus <royxu652@gmail.com>
2015-11-13 11:59:26 -0500

    response

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import urllib2,json
 from flask import Flask, render_template
+from websocket import create_connection
 
 app = Flask(__name__)
+token="xoxb-14528179959-cryA9x2tFb1SbDYppa0Xif4M"
 
-@app.route("/")
-def message():
-    client_id="14048866565.14274502486"
-    url="https://slack.com/oauth/authorize?client_id=%s&scope=channels:write"
-    url=url%(client_id)
+def connect():
+    url="https://slack.com/api/rtm.start?token=%s"
+    url=url%(token)
     request = urllib2.urlopen(url)
-    return "hi"
+    result = request.read()
+    r = json.loads(result)
+    return r["url"]
 
+def recieve():
+    ws = create_connection(connect())
+    while True:
+        result = ws.recv()
+        r = json.loads(result)
+        if r["type"] == "message" and "user" in r:
+            message(r["channel"], "hi")
+        print r
 
+def message(channel, text):
+    url="https://slack.com/api/chat.postMessage?token=%s&channel=%s&text=%s"
+    url=url%(token, channel, text)
+    request = urllib2.urlopen(url)
 
 if __name__ == "__main__":
-   app.debug = True
-   app.run(host="0.0.0.0", port=8000)
+    recieve()
