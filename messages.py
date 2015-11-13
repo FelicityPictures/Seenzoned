f48ad1d9f17d7ecd87f026b69158293307ddb2de
Live session user <felicitypictures@yahoo.com>
2015-11-13 02:56:15 +0000

    learned how to write file

--- messages.py
+++ messages.py
@@ -1,5 +1,15 @@
-import urllib2, json
+import urllib2, json, sys
 
 url = 'https://slack.com/api/channels.history'
 client_id = open('token.txt','r').read()
 
+msg = 'messages.txt'
+
+try:
+    messages = open(msg,'w')
+    print('creating')
+    messages.close()
+    print('done')
+except:
+    print('oh no :(')
+    sys.exit(0)
